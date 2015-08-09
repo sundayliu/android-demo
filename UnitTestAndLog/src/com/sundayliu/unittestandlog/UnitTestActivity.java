2a07eda527cfc4bdd8126f609efea07e33d737b3
sundayliu <sundayliu@tencent.com>
2015-08-09 19:57:50 +0800

    Init project UnitTestAndLog

---
+++ UnitTestAndLog/src/com/sundayliu/unittestandlog/UnitTestActivity.java
@@ -0,0 +1,15 @@
+package com.sundayliu.unittestandlog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UnitTestActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
