8165e2a1e063c79f7373cc45579b3409d43e4031
plumflower <plumflowerliu@gmail.com>
2015-10-11 19:19:21 +0800

    Intergate

--- Demo/src/com/sundayliu/demo/DemoArrayAdapter.java
+++ Demo/src/com/sundayliu/demo/DemoArrayAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,28 +14,26 @@ public class DemoArrayAdapter extends ArrayAdapter<String> {
     private final String[] values;
 
     public DemoArrayAdapter(Context context, String[] values) {
-      super(context, R.layout.listview_row1, values);
+      super(context, R.layout.listview_row_main, values);
       this.context = context;
       this.values = values;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-      LayoutInflater inflater = (LayoutInflater) context
-          .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      View rowView = inflater.inflate(R.layout.listview_row1, parent, false);
-      TextView textView = (TextView) rowView.findViewById(R.id.label);
-      ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-      textView.setText(values[position]);
-      // Change the icon for Windows and iPhone
-      String s = values[position];
-      if (s.startsWith("Windows7") || s.startsWith("iPhone")
-          || s.startsWith("Solaris")) {
-        imageView.setImageResource(R.drawable.ic_no);
-      } else {
-        imageView.setImageResource(R.drawable.ic_yes);
+      View row = convertView;
+      if (row == null){
+          LayoutInflater inflater = (LayoutInflater) context
+                  .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+          row = inflater.inflate(R.layout.listview_row_main, parent, false);
+          
+          ImageButton button = (ImageButton)row.findViewById(R.id.start);
+          String tag = values[position];
+          button.setTag(tag);
       }
-
-      return rowView;
+      
+      TextView text = (TextView)row.findViewById(R.id.label);
+      text.setText(values[position]);
+      return row;
     }
 }
