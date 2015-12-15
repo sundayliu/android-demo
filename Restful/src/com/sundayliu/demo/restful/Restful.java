86fc05b69245d4d2d824253d9bf53466244bb3c5
sundayliu <martinkro@aliyun.com>
2015-12-15 14:24:07 +0800

    Project Restful Android Client

---
+++ Restful/src/com/sundayliu/demo/restful/Restful.java
@@ -0,0 +1,15 @@
+package com.sundayliu.demo.restful;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Restful extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
